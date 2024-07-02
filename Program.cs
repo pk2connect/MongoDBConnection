069d03ef96fff7000a1e6f1b4d2ff0eff93c26c5
Prakash Karuppiah <prakashkrk1988@gmail.com>
2024-07-02 12:01:10 +0530

    Update Program.cs
    Code updated

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ namespace ConnectMongoDB
         {
 
             // Replace the connection string with your MongoDB Atlas URI.
-            var connectionString = "mongodb+srv://<YourUserName>:<YourPAssword>@testdatabase.ywvasfj.mongodb.net/?retryWrites=true&w=majority&appName=TestDatabase";
+            var connectionString = "mongodb+srv://<YourUserName>:<YourPAssword>@Your_DataBase_Name.ywvasfj.mongodb.net/?retryWrites=true&w=majority&appName=Your_DataBase_Name";
 
             // Create a MongoClient object.
             var client = new MongoClient(connectionString);
 
             // Get the database and collection.
-            var database = client.GetDatabase("testdatabase");
+            var database = client.GetDatabase("Your_DataBase_Name");
             var collection = database.GetCollection<BsonDocument>("your_collection_name");
 
             // Now you can perform operations on the collection.
