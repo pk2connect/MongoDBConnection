7d5aef5e441c04d32ca42b5030a68a48d6b97650
pk2connect <prakashkrk1988@gmail.com>
2024-07-02 11:58:03 +0530

    Commit to main
    Commit to main

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectMongoDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Replace the connection string with your MongoDB Atlas URI.
+            var connectionString = "mongodb+srv://prakashkrk1988:VoEmDNwwRSIEj2B8@testdatabase.ywvasfj.mongodb.net/?retryWrites=true&w=majority&appName=TestDatabase";
+
+            // Create a MongoClient object.
+            var client = new MongoClient(connectionString);
+
+            // Get the database and collection.
+            var database = client.GetDatabase("testdatabase");
+            var collection = database.GetCollection<BsonDocument>("your_collection_name");
+
+            // Now you can perform operations on the collection.
+        }
+    }
+}
